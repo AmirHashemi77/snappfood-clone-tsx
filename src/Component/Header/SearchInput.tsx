0671dfcfa6f2c0b327c40f9d3aa302ed0a015a10
AmirHashemi77 <amirhashemi539@gmail.com>
2024-08-12 20:55:44 +0330

    Create Header Component

---
+++ src/Component/Header/SearchInput.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent, FC, useState } from 'react';
+import style from './SearchInput.module.scss';
+import { useDispatch } from 'react-redux';
+
+interface PropsType{};
+
+const SearchInput:FC<PropsType> = () => {
+    const [timer,setTimer]=useState<ReturnType<typeof setTimeout>>()
+    const dispatch=useDispatch()
+    const searchHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+       
+        clearTimeout(timer)
+
+        const newTimer:ReturnType<typeof setTimeout>=setTimeout(()=>{
+            if(e.target.value && e.target.value.length>=3){
+                // dispatch(fetchSearchResultAction(e.target.value))
+            }
+        },1000)
+        setTimer(newTimer)
+    }
+
+    return (
+        <input type="text" id='search' onChange={searchHandler} className={style.searchInput} autoFocus/>
+
+    );
+};
+export default SearchInput;
