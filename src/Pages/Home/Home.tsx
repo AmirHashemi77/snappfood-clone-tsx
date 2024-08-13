fe3c28a8407ff3ac7f5ca677d566ad5fcf617b29
AmirHashemi77 <amirhashemi539@gmail.com>
2024-08-13 20:55:07 +0330

    add restrant category navbar

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,14 +1,38 @@
 import { FC } from 'react';
 import style from "./Home.module.scss"
+import Header from '../../Component/Header/Header';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../Store';
+import Footer from '../../Component/Footer/Footer';
+import RestrantCategory from '../../Component/RestrantCategory/RestrantCategory';
 
 interface PropsType{};
 
 const Home:FC<PropsType> = () => {
+  const loading=useSelector((state:RootState)=>state.ui.showLoading);
+    
   return (
-    <div>
+      <>
+          <Header/>
+         
+              {
+                  loading &&  <div className={style.loadingWrapper}><Loading/></div>
+              }
 
-    </div>
-
+          
+          
+          <div className={style.main}>
+          {!loading && <RestrantCategory/>}
+          <Slider title='برترین ها' tag='top' />
+          <Slider title='یک تجربه جدید' tag='newExperience' />
+          <Slider title='دارای تخفیف' tag='off'/>
+          {!loading &&  <AppBanner/>}
+          {!loading &&  <SignInBanner/>}
+         </div>
+         
+          <CitySection/>
+          <Footer/>
+      </>
   );
 };
 export default Home;
